0604340ee7284aa79cef2910453d180b8d2aabc9
Nitish <nitish27@gmail.com>
2017-04-14 18:53:14 -0400

    added some sample code.

--- src/ParserMapper.java
+++ src/ParserMapper.java
@@ -3,12 +3,33 @@
  */
 
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.io.Text;
-public class ParserMapper extends Mapper<Text, Text, Text, Text>  {
+
+public class ParserMapper extends Mapper<Text, Text, Text, Text> {
 
     @Override
     protected void map(Text key, Text value, Context context) throws IOException, InterruptedException {
+        findEmail(key, value, context);
+    }
 
+    private void findEmail(Text key, Text value, Context context) throws IOException, InterruptedException {
+        Pattern VALID_EMAIL_ADDRESS_REGEX =
+                Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+        Pattern valid_url = Pattern.compile("^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]", Pattern.CASE_INSENSITIVE);
+        String[] valueArray = value.toString().trim().split(" ");
+        for (String eachValue : valueArray) {
+            Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(eachValue);
+            Matcher urlMatcher = valid_url.matcher(eachValue);
+            if (matcher.find()) {
+                context.write(new Text("test"), new Text("email:"+eachValue));
+            }
+            if(urlMatcher.find()){
+                context.write(new Text("test"), new Text("link:"+eachValue));
+            }
+        }
     }
 }
--- src/ParserReducer.java
+++ src/ParserReducer.java
@@ -8,6 +8,11 @@ public class ParserReducer extends Reducer<Text, Text, Text, Text>  {
     @Override
     protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
         // TODO: please implement your reducer here
-
+        StringBuilder concatValues = new StringBuilder();
+        for (Text text : values) {
+            concatValues.append(" ");
+            concatValues.append(text.toString());
+        }
+        context.write(key, new Text(concatValues.toString().trim()));
     }
 }
