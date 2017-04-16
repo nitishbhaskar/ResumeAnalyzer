9ffb9e0894574cb6c44bfeb685e723771991e981
Nitish <nitish27@gmail.com>
2017-04-16 13:52:56 -0400

    Corrected the regex for matching URL

--- src/Utility.java
+++ src/Utility.java
@@ -17,7 +17,7 @@ public class Utility {
     public static Pattern VALID_EMAIL_ADDRESS_REGEX =
             Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
 
-    public static Pattern VALID_URL = Pattern.compile("^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]", Pattern.CASE_INSENSITIVE);
+    public static Pattern VALID_URL = Pattern.compile("^(https?|ftp|file)://?|www[.][-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]", Pattern.CASE_INSENSITIVE);
 
     public static String[] splitWithSpace(String inputString) {
         return inputString.toString().trim().split(" ");
