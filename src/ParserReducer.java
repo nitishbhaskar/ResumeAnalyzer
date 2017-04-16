da3600839abb4758eb151a8b043380d3e465328b
Nitish <nitish27@gmail.com>
2017-04-16 14:48:54 -0400

    Matching skills is assigned a score.

--- src/ParserReducer.java
+++ src/ParserReducer.java
@@ -1,18 +1,36 @@
 /**
  * Created by Nitish on 4/14/2017.
  */
+
 import java.io.IOException;
+import java.util.HashSet;
+
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.io.Text;
-public class ParserReducer extends Reducer<Text, Text, Text, Text>  {
+
+public class ParserReducer extends Reducer<Text, Text, Text, Text> {
+    HashSet<String> matchedSkills;
+
     @Override
     protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+        matchedSkills = new HashSet<>();
         // TODO: please implement your reducer here
         StringBuilder concatValues = new StringBuilder();
         for (Text text : values) {
+            String[] valueArray = text.toString().trim().split(":");
+            if (valueArray[0].equals("skill")) {
+                matchedSkills.add(valueArray[1]);
+            }
             concatValues.append(" ");
             concatValues.append(text.toString());
         }
+        concatValues.append(" ");
+        concatValues.append("matchedSkillScore:" + calculateMatchSkillScore());
         context.write(key, new Text(concatValues.toString().trim()));
+    }
+
+    private double calculateMatchSkillScore() {
+        Double score = (double) matchedSkills.size() / Utility.requiredSkills.keySet().size();
+        return Double.parseDouble(Utility.df.format(score * 100));
     }
 }
